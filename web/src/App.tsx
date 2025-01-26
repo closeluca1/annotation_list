46ccdf846339ec9458849c768bf03aed95d1e9ba
Patrick <contatopatricklucas@gmail.com>
2025-01-26 02:08:40 -0300

    fixed tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import ScrollToTop from "./shared/utils/scrollToTop";
 
+import { CalendarPage } from "./views/public/calendar";
+import { ReminderPage } from "./views/public/reminder";
+import { TodayPage } from "./views/public/today";
+
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<CalendarPage />} />
 
-export default App
+        {/* <Route path="/lista-de-tarefas" element={<CalendarPage />} /> */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
